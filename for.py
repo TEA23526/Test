c373c496cb0ab1eb2ca8ce8ac0a46e1678828942
NetAcad DEVASC <changeme@example.com>
2024-11-14 15:49:07 +0000

    --Sortieren Ruter und Switches

--- for.py
+++ for.py
@@ -1,5 +1,13 @@
 devices=["R1","R2","R3","S1","S2"] 
+switche=[]
+ruter=[]
 for item in devices: 
     
-    if "R" in item:
+    if "S" in item:
+        switche.append(item)
         print(item) 
+    elif "R" in item:
+        ruter.append(item)
+
+print(f"meine Scitche: {switche}")
+print(f"Meine Ruter: {ruter}")
